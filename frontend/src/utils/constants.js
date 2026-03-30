a2742880fde3da350256fff38e4c248e98b6614b
KhushiYadav18 <yadavkhushi00045@gmail.com>
2026-03-30 21:58:01 +0530

    api base url change

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000/api'
+export const API_BASE_URL = 'https://merge-system-its.onrender.com/api'
 
 export const ACCESS_TOKEN_KEY = 'adaptive_access_token'
 export const REFRESH_TOKEN_KEY = 'adaptive_refresh_token'
