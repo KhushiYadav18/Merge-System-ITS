74cfcf8c738fed59711366b4385768325868ed27
KhushiYadav18 <yadavkhushi00045@gmail.com>
2026-03-30 23:29:01 +0530

    url change

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://merge-system-its.onrender.com/api'
+export const API_BASE_URL = 'merge-system-its-production.up.railway.app/api'
 
 export const ACCESS_TOKEN_KEY = 'adaptive_access_token'
 export const REFRESH_TOKEN_KEY = 'adaptive_refresh_token'
